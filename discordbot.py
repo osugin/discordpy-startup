17be4fd88e6171b001fe4925b50ad2e4f944f6ce
osugin <52478899+osugin@users.noreply.github.com>
2019-07-05 00:01:58 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,10 +25,27 @@ async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
-    # 「/じー」と発言したら「じ、じろじろ見てんじゃないわよ！！ぶっ殺すわよ！？」が返る処理
+    # 「/じー」と発言したら「こっち見んな！！ぶっ殺すわよ！？」が返る処理
     if message.content == '/じー':
-        await message.channel.send('じ、じろじろ見てんじゃないわよ！！ぶっ殺すわよ！？')
-# 話しかけた人に返信する
+        await message.channel.send('こっち見んな！！ぶっ殺すわよ！？')
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/キャルちゃん」と発言したら「なによ！なんか用？」が返る処理
+    if message.content == '/キャルちゃん':
+        await message.channel.send('なによ！なんか用？')
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/ねぇねぇ」と発言したら「触んな！」が返る処理
+    if message.content == '/ねぇねぇ':
+        await message.channel.send('触んな！')
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/つかれた」と発言したら「ゆっくりする？」が返る処理
+    if message.content == '/つかれた':
+        await message.channel.send('ゆっくりする？')
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)

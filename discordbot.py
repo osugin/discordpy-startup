41fbf2b25e88e0a09e191f2ef7d8f4a8b79550e5
osugin <52478899+osugin@users.noreply.github.com>
2019-07-10 01:53:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -100,7 +100,62 @@ async def on_message(message):
             # メッセージが送られてきたチャンネルへメッセージを送ります
             await message.channel.send(m)
 
+"""メンバー募集 (.rect@数字)"""
+if message.content.startswith(".rect"):
+mcount = int(message.content[6:len(message.content)])
+text= "あと{}人 募集中\n"
+revmsg = text.format(mcount)
+#friend_list 押した人のList
+frelist = []
+msg = await client.send_message(message.channel, revmsg)
+    #投票の欄
+    await client.add_reaction(msg, '\u21a9')
+    await client.add_reaction(msg, '⏫')
+    await client.pin_message(msg)
 
+    #リアクションをチェックする
+    while len(frelist) < int(message.content[6:len(message.content)]):
+        target_reaction = await client.wait_for_reaction(message=msg)
+        #発言したユーザが同一でない場合 真
+        if target_reaction.user != msg.author:
+            #==============================================================
+            #押された絵文字が既存のものの場合 >> 左　del
+            if target_reaction.reaction.emoji == '\u21a9':
+                #==========================================================
+                #◀のリアクションに追加があったら反応 frelistにuser.nameがあった場合　真
+                if target_reaction.user.name in frelist:
+                    frelist.remove(target_reaction.user.name)
+                    mcount += 1
+                    #リストから名前削除
+                    await client.edit_message(msg, text.format(mcount) +
+                                                    '\n'.join(frelist))
+                        #メッセージを書き換え
+
+                else:
+                    pass
+            #==============================================================
+            #押された絵文字が既存のものの場合　>> 右　add
+            elif target_reaction.reaction.emoji == '⏫':
+                if target_reaction.user.name in frelist:
+                    pass
+
+                else:
+                    frelist.append(target_reaction.user.name)
+                    #リストに名前追加
+                    mcount = mcount - 1
+                    await client.edit_message(msg, text.format(mcount) +
+                                                    '\n'.join(frelist))
+
+
+            elif target_reaction.reaction.emoji == '✖':
+                    await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
+                    await client.unpin_message(msg)
+                    break
+            
+            #ユーザーがつけたリアクションを消す※権限によってはエラー
+            #==============================================================
+    else:
+        await client.edit_message(msg, '募集終了\n'+ '\n'.join(frelist))
 
 # クラバトについてのコード
     CHANNEL_ID = 596583155578961935

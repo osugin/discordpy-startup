be2479c11beb41b452e6410673eb4f9848174872
osugin <52478899+osugin@users.noreply.github.com>
2019-07-09 12:52:29 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -87,7 +87,7 @@ async def on_message(message):
         # 送り主がBotだった場合反応したくないので
         if client.user != message.author:
             # メッセージを書きます
-            m = " message.author.name + " おつかれさま！今日もよくがんばったわね！ゆっくり休むのよ！！"
+            m = "おつかれさま！" + message.author.name + "今日もよくがんばったわね！ゆっくり休むのよ！！"
             # メッセージが送られてきたチャンネルへメッセージを送ります
             await message.channel.send(m)
 
